84f828940a98e9a0c3a618c3ea2442b3d54d477e
abi.fc <abi.fc@fingent.com>
2020-08-07 11:56:04 +0530

    products added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 // Feature 1
 import React from "react";
+import data from "./data.json";
+import Products from "./components/Products";
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>Main</main>
-      <footer>All right is reserved.</footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">cart</div>
+          </div>
+        </main>
+        <footer>All right is reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
